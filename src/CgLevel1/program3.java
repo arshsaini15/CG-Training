53b361fab0fdaac4f349e59ab01b3ea154cab6cb
arshsaini15 <arshdeepp015@gmail.com>
2025-12-31 12:21:44 +0530

    Level 1 - Program 3

---
+++ src/CgLevel1/program3.java
@@ -0,0 +1,12 @@
+package CgLevel1;
+
+public class program3 {
+        public static void main(String[] args) {
+        double kilometers = 10.8;
+        double conversionFactor = 1.6;
+
+        double miles = kilometers * conversionFactor;
+
+        System.out.println("The distance " + kilometers + " in miles is " + miles);
+    }
+}
