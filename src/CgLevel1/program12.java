fb4b160bddc97e384f48725d42ad4663574be35a
arshsaini15 <arshdeepp015@gmail.com>
2026-01-01 10:51:09 +0530

    Level 1 - Program 12

---
+++ src/CgLevel1/program12.java
@@ -0,0 +1,16 @@
+package CgLevel1;
+
+import java.util.Scanner;
+
+public class program12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter height: ");
+        double height = sc.nextDouble();
+
+        double area = 0.5 * base * height;
+        System.out.println("The area of the triangle is " + area);
+    }
+}
