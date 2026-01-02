42ff54d17bce228ed146b13f47b4046d4218448f
arshsaini15 <arshdeepp015@gmail.com>
2026-01-02 20:58:05 +0530

    Level 1 : Power of a Number (For Loop)

---
+++ src/Level2/p12.java
@@ -0,0 +1,16 @@
+package Level2;
+
+import java.util.Scanner;
+
+public class p12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int power = sc.nextInt();
+        long result = 1;
+        for (int i = 1; i <= power; i++) {
+            result *= number;
+        }
+        System.out.println("Result: " + result);
+    }
+}
