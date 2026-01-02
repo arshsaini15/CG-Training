50936a3e55175e60914d4b88a7980e081aa526de
arshsaini15 <arshdeepp015@gmail.com>
2026-01-02 11:36:28 +0530

    Level 1 : Counter

---
+++ src/Level1/p8.java
@@ -0,0 +1,14 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class p8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = sc.nextInt();
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
+    }
+}
