6ff655f654750d390825eca08e4eda518cc1ae55
arshsaini15 <arshdeepp015@gmail.com>
2026-01-17 03:39:03 +0530

    Book

---
+++ src/Level1/Book.java
@@ -0,0 +1,23 @@
+package Level1;
+
+public class Book {
+    private String title;
+    private String author;
+    private int price;
+
+    Book() {
+        System.out.println("Default constructor");
+    }
+    Book(String title, String author, int price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+
+        System.out.println(this.title + " " + this.author + " " + this.price);
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        Book book2 = new Book("Abc", "Def",3);
+    }
+}
