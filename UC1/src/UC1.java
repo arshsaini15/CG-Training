bd1a1fbcdbe9219bfdccce515ee9744cfa2a938a
arshsaini15 <12345678+arshsaini015@users.noreply.github.com>
2026-03-18 10:15:42 +0530

    UC1

---
+++ UC1/src/UC1.java
@@ -0,0 +1,36 @@
+public class UC1 {
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+        Feet f3 = new Feet(2.0);
+
+        System.out.println("f1 vs f2: " + f1.equals(f2)); // true
+        System.out.println("f1 vs f3: " + f1.equals(f3)); // false
+    }
+}
